f0be161c353798d45f56a1c6746b7029120cb501
dhruvghulati <ghulatid@gmail.com>
2016-03-11 21:31:41 +0000

    Initial veridu verification

--- www/js/app.js
+++ www/js/app.js
@@ -8,72 +8,76 @@
 require('angular');
 require('veridu-angularjs-sdk');
 
-angular.module('starter', ['ionic', 'starter.controllers','starter.services', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'veridu.angularjs.sdk', 'starter.services', 'firebase'])
     .constant('FirebaseUrl', 'https://ionicle.firebaseio.com/')
     .service('rootRef', ['FirebaseUrl', Firebase])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider, VeriduProvider) {
+        $stateProvider
+
+        VeriduProvider.client = 'demo';
+        VeriduProvider.API_VERSION = 'rafael';
+
+        .
+        state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'AppCtrl'
+        })
+
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html'
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/browse.html'
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/playlists');
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,58 +1,87 @@
 angular.module('starter.controllers', [])
 
+.controller('VeriduCtrl', VeriduCtrl);
+VeriduCtrl.$inject = ['$scope', 'Veridu'];
+function VeriduCtrl($scope, Veridu) {
+    var vm = this;
+    vm.Veridu = Veridu;
+    if (!Veridu.cfg)
+        window.alert('You need to configure your Veridu credentials on the source code of this page, Line 111, 112 and 113');
+    this.getProfile = function () {
+        Veridu.API.fetch('GET', 'profile/' + Veridu.cfg.user).then(
+            function success(response) {
+                vm.profile = response.data;
+            },
+            function error() {
+                vm.profile = 'Error fetchng the API'
+            }
+        );
+    };
+    this.getFacts = function () {
+        Veridu.API.fetch('GET', 'facts/' + Veridu.cfg.user).then(
+            function success(response) {
+                vm.facts = response.data;
+            },
+            function error() {
+                vm.facts = 'Error fetching the API';
+            }
+        );
+    };
+    window.SDK = Veridu;
+}
 
+.
+controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeLogin();
+        }, 1000);
+    };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+    });
